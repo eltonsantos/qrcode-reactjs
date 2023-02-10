d067f24bcf3a1039f60e5078b96171cc13747112
Elton Santos <eltonaxl@hotmail.com>
2023-02-10 03:34:59 -0300

    Finish app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import QRCodeLink from "qrcode";
+import { useState } from "react";
+import QRCode from "react-qr-code";
 import "./App.css";
 
 function App() {
+  const [link, setLink] = useState("");
+  const [qrcodeLink, setQrcodeLink] = useState("");
+
+  function handleGenerate(link_url) {
+    QRCodeLink.toDataURL(
+      link_url,
+      {
+        width: 660,
+        margin: 3,
+      },
+      function (err, url) {
+        setQrcodeLink(url);
+      }
+    );
+  }
+
+  function handleQrcode(e) {
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  }
+
   return (
     <div className="container">
-      <input className="input" type="text" placeholder="Digite seu link..." />
+      <QRCode value={link} />
+
+      <input
+        className="input"
+        value={link}
+        onChange={(e) => handleQrcode(e)}
+        placeholder="Digite seu link..."
+      />
+
+      <a href={qrcodeLink} download={`qrcode.png`}>
+        Baixar QRCode
+      </a>
     </div>
   );
 }
